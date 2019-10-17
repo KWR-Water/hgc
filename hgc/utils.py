bd2a80ffebe753738ca6c79d44c2d399864a884e
funky9000 <vonker@nwg.local>
2019-10-17 14:59:00 +0200

    Scaffolding

---
+++ hgc/utils.py
@@ -0,0 +1,21 @@
+
+def test_something(a, b, c):
+    """
+    Test if something works, especially the documentation
+
+    Parameters
+    ----------
+    a: str
+    b: int
+
+    Returns
+    -------
+    c: int
+        Approximate answer to this advanced calculation.
+
+    Notes
+    -----
+    Bladie bladie
+    """
+
+    return int(a) + int(b)

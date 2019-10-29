837f1d7c03d696304d81d33d0dad4e69a1d15eed
Martin <martin.korevaar@kwrwater.nl>
2019-10-29 15:28:37 +0100

    Tests work with pytest now

--- tests/test_samples_frame.py
+++ tests/test_samples_frame.py
@@ -1,39 +1,17 @@
-# from hgc import samples_frame
 import hgc
+from hgc.samples_frame import SamplesFrame
 import pandas as pd
 import numpy as np
 from unittest import TestCase, mock
 from datetime import datetime
-from hgc.samples_frame import SamplesFrame
 import pytest
 
 
-class testSamplesFrame(TestCase):
-    """ Simple tests for SampleFrame """
-    def setUp(self):
-        super().setUp()
-        with mock.patch.object(SamplesFrame, "_validate") as mocked_validate:
-            mocked_validate.return_value = True
-            df = pd.DataFrame()
-        self.df = df
-
-    def testHgcNamespaceIsAdded(self, ):
-        ''' Test whether the HGC methods are added to
-            dataframes that are created
-        '''
-            self.df.hgc
-
-    def testUnitsAreConverted(self):
-        ''' test the method that converts units '''
-        self.df.hgc.convert_to_standard_units()
-        raise NotImplementedError()#a
-
-class testSamplesFrame2(TestCase):
-    """docstring for testSamplesFrame."""
-    def setUp(self):
-        super().setUp()
-        dates = [datetime(1990, _m + 1, 1) for _m in range(10)]
-        self.df = pd.DataFrame(dict(date=dates,
-                                    hco3=np.arange(10),
-                                    fe=np.arange(10) + 10,
-                                    temp=np.arange(10) + 20))
+def testHgcNamespaceIsAdded():
+    ''' Test whether the HGC methods are added to
+        dataframes that are created
+    '''
+    with mock.patch.object(SamplesFrame, "_validate") as mocked_validate:
+        mocked_validate.return_value = True
+        df = pd.DataFrame()
+        df.hgc

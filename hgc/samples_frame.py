bd1258e60e9ef88c3c835bc971813f7437b90a14
Martin <martin.korevaar@kwrwater.nl>
2019-10-29 14:43:54 +0100

    Added mocking to test

--- hgc/samples_frame.py
+++ hgc/samples_frame.py
@@ -1,12 +1,34 @@
-# https://github.com/ContinuumIO/cyberpandas/blob/master/cyberpandas/ip_array.py
-# https://pandas.pydata.org/pandas-docs/stable/development/extending.html
+''' In here the extension of the pandas DataFrame is defined. It adds methods
+    for validation, calculation of properties. Extending the dataframe is based on:
+    # https://github.com/ContinuumIO/cyberpandas/blob/master/cyberpandas/ip_array.py
+    # https://pandas.pydata.org/pandas-docs/stable/development/extending.html
+'''
 
 import pandas as pd
 
 @pd.api.extensions.register_dataframe_accessor("hgc")
-class SamplesFrame:
+class SamplesFrame(object):
+    ''' This class defines the extensions to a pandas dataframe
+        that are specific for this package. The extension is effectuated
+        by calling the class decorator `@pd.api.extensions.register_dataframe_accessor("hgc")`
+        All methods and attributes defined in this class `SamplesFrame` is available
+        in the namespace `hgc` of the Dataframe.
+
+        Example
+
+        ```
+          import pandas as pd
+          import hgc
+
+          # define dataframe
+          df = pd.DataFrame({'a': [1,2,3], 'b': [11,12,13]})
+          # because hgc is also imported above, the namespace
+          # `hgc` came available to the dataframe. This allows
+          # to call the following method
+          df.hgc.validate()
+    '''
     def __init__(self, pandas_obj):
-        # self._validate(pandas_obj)
+        self._validate(pandas_obj)
         self._obj = pandas_obj
 
     @staticmethod
--- tests/test.py
+++ tests/test.py
@@ -1,13 +1,39 @@
-# from hgc import sample_frame
+# from hgc import samples_frame
 import hgc
 import pandas as pd
-from unittest import TestCase
+import numpy as np
+from unittest import TestCase, mock
+from datetime import datetime
+from hgc.samples_frame import SamplesFrame
+import pytest
+
 
 class testSamplesFrame(TestCase):
-    """docstring for testSamplesFrame."""
+    """ Simple tests for SampleFrame """
+    def setUp(self):
+        super().setUp()
+        with mock.patch.object(SamplesFrame, "_validate") as mocked_validate:
+            mocked_validate.return_value = True
+            df = pd.DataFrame()
+        self.df = df
+
     def testHgcNamespaceIsAdded(self, ):
         ''' Test whether the HGC methods are added to
             dataframes that are created
         '''
-        df = pd.DataFrame(dict(a=[1,2], b=[3,4]))
-        df.hgc.convert_to_standard_units()
+            self.df.hgc
+
+    def testUnitsAreConverted(self):
+        ''' test the method that converts units '''
+        self.df.hgc.convert_to_standard_units()
+        raise NotImplementedError()#a
+
+class testSamplesFrame2(TestCase):
+    """docstring for testSamplesFrame."""
+    def setUp(self):
+        super().setUp()
+        dates = [datetime(1990, _m + 1, 1) for _m in range(10)]
+        self.df = pd.DataFrame(dict(date=dates,
+                                    hco3=np.arange(10),
+                                    fe=np.arange(10) + 10,
+                                    temp=np.arange(10) + 20))

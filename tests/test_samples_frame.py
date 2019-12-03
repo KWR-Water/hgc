8a8b8ad192830ba638de4a75b8a44ee5b0647bb6
funky9000 <vonker@nwg.local>
2019-12-03 17:23:00 +0100

    Validation, make valid and ratios

--- tests/test_samples_frame.py
+++ tests/test_samples_frame.py
@@ -1,3 +1,6 @@
+import sys
+import logging
+
 import hgc
 from hgc.samples_frame import SamplesFrame
 import pandas as pd
@@ -16,8 +19,35 @@ def test_hgc_namespace_is_added():
         df = pd.DataFrame()
         df.hgc
 
-def test_validate_dataframe():
-    """ Df validation """
 
-    df = pd.read_excel('./examples/data/dataset_basic.xlsx')
-    #df.validate()
+def test_init_samples_frame():
+    #caplog.set_level(logging.INFO)
+    #logging.basicConfig(stream=sys.stdout, format='%(asctime)s %(message)s', level=logging.DEBUG)
+    #logging.getLogger().addHandler(logging.StreamHandler())
+    df = pd.read_excel('./examples/data/dataset_invalid_columns.xlsx', skiprows=[1])
+    assert df.hgc.is_valid == False
+    
+
+def test_make_valid(): 
+    df = pd.read_excel('./examples/data/dataset_invalid_columns.xlsx', skiprows=[1])
+    df.hgc.make_valid()
+
+    assert df.hgc.is_valid == True
+
+
+def test_get_ratios_invalid_frame():
+    df = pd.DataFrame() 
+    with pytest.raises(ValueError):
+        df.hgc.get_ratios()
+
+
+def test_get_ratios():
+    df = pd.read_excel('./examples/data/dataset_basic.xlsx', skiprows=[1])
+    df_ratios = df.hgc.get_ratios()
+
+    assert isinstance(df_ratios, pd.core.frame.DataFrame)
+
+
+def test_consolidate():
+    df = pd.read_excel('./examples/data/dataset_basic.xlsx', skiprows=[1])
+    df.consolidate()

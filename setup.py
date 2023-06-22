b9c5ef03d1a1ba16e87c366341586d29e87d695e
Martin Korevaar <martinwk@users.noreply.github.com>
2023-06-22 12:04:28 +0200

    bump minor version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 setup(
     name='hgc',
-    version='0.3.1',
+    version='0.3.2',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='A python package for correction, validation and analysis of ground water quality samples',
